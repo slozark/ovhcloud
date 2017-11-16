ebcdf720c5351a2eb1167207fb6cbd1c44cbf90b
devroede <sylvain.devroede@etudiant.univ-lille1.fr>
2017-11-16 17:27:43 +0100

    Parsing args

--- ovhcloud/client.py
+++ ovhcloud/client.py
@@ -1,22 +1,25 @@
-import sys
-
+import argparse
 import ovh
-
 import ovhcloud
 
 
 class Launcher(object):
 
-    def __init__(self, args, _configuration_dir=None):
+    def __init__(self, args, _configuration_dir=None, _configuration_file=None):
         self._configuration_dir = ovhcloud.DEFAULT_CONFIGURATION_DIR \
             if _configuration_dir is None else _configuration_dir
 
-        #Uses ovh.conf file
-        self._ovhclient = ovh.Client(config_file='./ovh.conf3')
+        self._configuration_file = ovhcloud.DEFAULT_CONFIGURATION_DIR + '/ovh.conf' \
+            if _configuration_file is None else _configuration_file
+
 
     @property
     def configuration_dir(self):
         return self._configuration_dir
+
+    @property
+    def configuration_file(self):
+        return self._configuration_file
 
     @property
     def cache_file(self):
@@ -26,13 +29,34 @@ class Launcher(object):
     def ovhclient(self):
         return self._ovhclient
 
-
+    def start(self):
+        self._ovhclient = ovh.Client(config_file=self.configuration_file)
 
 
 def main():
-    client = Launcher(sys.argv[1:])
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--conf-dir', dest="conf_dir")
+    parser.add_argument('-c', '--conf-ovh', dest='conf_ovh')
+    parser.add_argument('-a', '--api', dest='ovh_com')
+    args = parser.parse_args()
+
+    client = Launcher(_configuration_dir=args.conf_dir,_configuration_file=args.conf_ovh)
+    client.start()
+
+
+    ovhcloud.checkCache(args.ovh_conf)
+
+
+
+    test = client.ovhclient.get(_target="/me")
+    print(test)
+
+    '''
+    json_ret = client.ovhclient.get(ovh_com)
 
     print(client.ovhclient.get('/me'))
+    '''
 
 if __name__ == '__main__':
     main()
