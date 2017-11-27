0f5829cfe6adb7b126e70a229fdb1b50ee53efde
devroede <sylvain.devroede@etudiant.univ-lille1.fr>
2017-11-27 12:56:45 +0100

    First step to parse args and rest options

--- ovhcloud/__init__.py
+++ ovhcloud/__init__.py
@@ -7,8 +7,10 @@ from pkg_resources import get_distribution, resource_filename
 __version__ = '0.1'
 
 
+#Global values
 DEFAULT_CONFIGURATION_DIR = os.path.expanduser('~/.ovhcloud/')
-
 OVH_API_URL = 'https://api.ovh.com/1.0'
+REST_METHODS = ['get','put','post','delete']
 
+#Logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
--- ovhcloud/client.py
+++ ovhcloud/client.py
@@ -1,16 +1,22 @@
 import argparse
 import ovh
-
 import ovhcloud
+
+class ArgumentError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
 
 
 class Launcher(object):
-    def __init__(self, _configuration_dir=None, _configuration_file=None):
+    def __init__(self, _args=None):
         self._configuration_dir = ovhcloud.DEFAULT_CONFIGURATION_DIR \
-            if _configuration_dir is None else _configuration_dir
+            if _args.conf_dir is None else _args.conf_dir
 
         self._configuration_file = ovhcloud.DEFAULT_CONFIGURATION_DIR + '/ovh.conf' \
-            if _configuration_file is None else _configuration_file
+            if _args.conf_ovh is None else _args.conf_ovh
 
         self._ovhApi = ovh.Client(config_file=self.configuration_file)
 
@@ -30,27 +36,47 @@ class Launcher(object):
     def ovhApi(self):
         return self._ovhApi
 
+    @property
+    def requestUrl(self):
+        return self._requestUrl
+
+
 '''
     def start(self):
         cache = CacheManager(self)
         cache.checkCache()
 '''
 
-def main():
-    from cache_manager import CacheManager
+
+def check_args():
+    # Should I add an arg to force cache cleaning ?
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--conf-dir', dest="conf_dir")
     parser.add_argument('-c', '--conf-ovh', dest='conf_ovh')
     parser.add_argument('-a', '--api', dest='cli_req')
+    parser.add_argument('-m', '--method', dest='rest_method')
     args = parser.parse_args()
 
-    # Should I add an arg to force cache cleaning ?
+    #if (True):
+     #   raise ArgumentError('test')
 
-    client = Launcher(_configuration_dir=args.conf_dir, _configuration_file=args.conf_ovh)
+    return args
 
-    cache = CacheManager(client)
-    cache.checkCache(args.cli_req)
+
+def main():
+    from cache_manager import CacheManager
+
+    try:
+        args = check_args()
+        client = Launcher(args)
+
+        cache = CacheManager(client)
+        cache.checkCache(args.cli_req)
+
+    except ArgumentError as e:
+        print("Invalid argument %s, aborting." % (e.value))
+        exit(1)
 
 
 if __name__ == '__main__':
